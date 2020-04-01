498b9eb4a3d5ae8966f6bea9534cc346c8a66bd9
hellostronger <48884346+hellostronger@users.noreply.github.com>
2020-04-01 11:22:59 +0800

    Update getopnum.java

--- 结对作业/src/Dao/getopnum.java
+++ 结对作业/src/Dao/getopnum.java
@@ -2,18 +2,15 @@ package Dao;
 
 import java.security.SecureRandom;
 
-/*
- * 生成随机数用于式子
- */
 public class getopnum {
-	public  int number(int limit,int min) {	//min < 返回的随机数  < limit
-		SecureRandom r = new SecureRandom();
-		int i=0;
-		while(i<=min) {
-			i = r.nextInt();
-			i = i%limit;
-			if(i<0)	i = -i;			
-		}
-		return i;
-	}
+	public  int number(int limit,int min) { //min < 返回的随机数  < limit
+		  SecureRandom r = new SecureRandom();
+		  int i=0;
+		  while(i<=min) {
+		   i = r.nextInt();
+		   i = i%limit;
+		   if(i<0) i = -i;}
+		  return i;
+		 }
+
 }
