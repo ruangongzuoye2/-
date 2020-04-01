9b89a8439a002932f1c93987da317dd34a639e0e
hellostronger <48884346+hellostronger@users.noreply.github.com>
2020-04-01 11:22:55 +0800

    Update information.java

--- 结对作业/src/unity/information.java
+++ 结对作业/src/unity/information.java
@@ -1,19 +1,14 @@
 package unity;
 
+import java.io.File;
+
 public class information {
-	 public static boolean isOp() {
-		return op;
-	}
-	public static void setOp(boolean op) {
-		information.op = op;
-	}
-	static int num_timu;//生成题目数
-	 static  int resultmax;//结果最大值
+	 static int num_timu;//生成题目数
 	 static  int fenmu_max;//分母最大值
 	 static  int nature;//式子中自然数最大值
 	 static String problem;//题目文件
 	 static String answer;//答案文件
-	 static boolean op;//确定引用文件数据还是存储数据到文件
+	 static String file_s;//检验答案结果保存的文件
 	
 	public static int getNum_timu() {
 		return num_timu;
@@ -33,12 +28,6 @@ public class information {
 	public static void setNum_timu(int num_timu) {
 		information.num_timu = num_timu;
 	}
-	public static int getResultmax() {
-		return resultmax;
-	}
-	public static void setResultmax(int resultmax) {
-		information.resultmax = resultmax;
-	}
 	public static int getFenmu_max() {
 		return fenmu_max;
 	}
@@ -51,5 +40,12 @@ public class information {
 	public static void setNature(int nature) {
 		information.nature = nature;
 	}
+	public static String getFile_s() {
+		return file_s;
+	}
+	public static void setFile_s(String file_s) {
+		information.file_s = file_s;
+	}
+	
 	
 }
